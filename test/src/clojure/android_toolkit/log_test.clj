6c53b7f70b787c4b83171c7d541aeca072b14d23
Daniel Solano Gómez <clojure@sattvik.com>
2011-03-07 14:34:35 -0600

    Add unit tests

---
+++ test/src/clojure/android_toolkit/log_test.clj
@@ -0,0 +1,98 @@
+; Copyright © 2011 Sattvik Software & Technology Resources, Ltd. Co.
+; All rights reserved.
+;
+; This program and the accompanying materials are made available under the
+; terms of the Eclipse Public License v1.0 which accompanies this distribution,
+; and is available at <http://www.eclipse.org/legal/epl-v10.html>.
+;
+; By using this software in any fashion, you are agreeing to be bound by the
+; terms of this license.  You must not remove this notice, or any other, from
+; this software.
+
+(ns android-toolkit.log-test
+  "Tests the functionality of the clojure.contrib.android.log namespace"
+  {:author "Daniel Solano Gómez"}
+  (:gen-class :main false
+              :extends junit.framework.TestCase
+              :methods [[testLogDebug [] void]
+                        [testLogVerbose [] void]
+                        [testLogInfo [] void]
+                        [testLogWarn [] void]
+                        [testLogError [] void]]
+              :exposes-methods {setUp superSetUp})
+  (:import android.os.SystemClock)
+  (:use android-toolkit.log
+        clojure.java.io
+        clojure.java.shell
+        junit.assert))
+
+(def tag "LogTest")
+
+(deflog tag)
+
+(def start-signal (atom false))
+
+(defn- collect
+  [text]
+  (future
+    (let [process (..
+                    (ProcessBuilder. ["logcat" "-s" tag])
+                    (redirectErrorStream true)
+                    start)
+          pattern (re-pattern text)
+          start   (SystemClock/elapsedRealtime)
+          timed-out? (fn [] (< 500 (- (SystemClock/elapsedRealtime) start)))]
+      (binding [*in* (reader (.getInputStream process))]
+        (loop [line (read-line)]
+          (cond
+            (or (nil? line)
+                (timed-out?)) nil
+            (and @start-signal
+                 (re-find pattern line)) line
+            :else (recur (read-line))))))))
+
+(defn- start-collecting []
+  (compare-and-set! start-signal false true))
+
+(defn logged-with-prefix?
+  [text prefix]
+  (is (re-find (re-pattern (str \^ prefix \/ tag)) text)))
+
+(defn -setUp [this]
+  (.superSetUp this)
+  (reset! start-signal false))
+
+(defn -testLogDebug [this]
+  (let [test-text "This is a test debug message."
+        collector (collect test-text)]
+    (start-collecting)
+    (log-d test-text)
+    (logged-with-prefix? @collector "D")))
+
+(defn -testLogVerbose [this]
+  (let [test-text "This is a test verbose message."
+        collector (collect test-text)]
+    (start-collecting)
+    (log-v test-text)
+    (logged-with-prefix? @collector "V")))
+
+(defn -testLogInfo [this]
+  (let [test-text "This is a test info message."
+        collector (collect test-text)]
+    (start-collecting)
+    (log-i test-text)
+    (logged-with-prefix? @collector "I")))
+
+(defn -testLogWarn [this]
+  (let [test-text "This is a test warn message."
+        collector (collect test-text)]
+    (start-collecting)
+    (log-w test-text)
+    (logged-with-prefix? @collector "W")))
+
+(defn -testLogError [this]
+  (let [test-text "This is a test error message."
+        collector (collect test-text)]
+    (start-collecting)
+    (log-e test-text)
+    (logged-with-prefix? @collector "E")))
---
+++ test/src/clojure/junit/assert.clj
@@ -0,0 +1,46 @@
+; Copyright © 2011 Sattvik Software & Technology Resources, Ltd. Co.
+; All rights reserved.
+;
+; This program and the accompanying materials are made available under the
+; terms of the Eclipse Public License v1.0 which accompanies this distribution,
+; and is available at <http://www.eclipse.org/legal/epl-v10.html>.
+;
+; By using this software in any fashion, you are agreeing to be bound by the
+; terms of this license.  You must not remove this notice, or any other, from
+; this software.
+
+(ns junit.assert
+  "A small namespace to help with the tediousness of having to use JUnit
+  assertions."
+  {:author "Daniel Solano Gómez"}
+  (:import junit.framework.Assert))
+
+(defn is
+  [actual]
+  (Assert/assertTrue (boolean actual)))
+
+(defn is-not
+  [actual]
+  (Assert/assertFalse (boolean actual)))
+
+(defn is-eq
+  [expected actual]
+  (Assert/assertEquals expected actual))
+
+(defn is-not-same
+  [expected actual]
+  (Assert/assertNotSame expected actual))
+
+(defn is-same
+  [expected actual]
+  (Assert/assertSame expected actual))
+
+(defmacro does-throw
+  [ex-class form]
+  `(try
+     ~form
+     (Assert/fail (str "Exception " ~ex-class " not thrown."))
+     (catch ~ex-class _#
+       nil)
+     (catch Throwable e#
+       (Assert/fail (str "Wrong exception " (class e#) " caught.")))))
